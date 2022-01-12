f2f13bc516a72fec40dbccc82ebb463bb3d37a64
eslywadan <esly.wadan@gmail.com>
2022-01-12 15:37:31 +0800

    add engineering.py

---
+++ engineering.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from flask_restplus import Api
+
+eng_bd = Blueprint('eng_api', __name__)
+eng_api = Api(eng_bd)
+
+@eng_bd.route('/hello/')
+def hello():
+  return "Hello from ENG Page"
