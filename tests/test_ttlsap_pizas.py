2e0476f0b7b5e717c1f0fe9410a75cf16b51ae48
eslywadan <esly.wadan@gmail.com>
2022-02-14 22:07:21 +0800

    del ../tests/test_ttlsap_piza.py

--- tests/test_ttlsap_pizas.py
+++
@@ -1,7 +0,0 @@
-from nose.tools import assert_equal
-from ttlsap.pizas import *
-
-def test_ttlsap_pizas():
-    """ test the pizas """
-    pizas = Pizas()
-    res = pizas.query_graph(pizas.g, sub="ex1", predicate=None, obj=Literal('pos'))
