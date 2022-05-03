6b36fa162e32c9127c070336f0f84caca32e362b
qs.chou@innolux.com <qs.chou@innolux.com>
2022-05-03 10:37:25 +0800

    mergeg main api_portal.py

--- api_portal.py
+++ api_portal.py
@@ -1,15 +1,7 @@
 from flask import Flask
 import werkzeug 
 import tools.request_handler as req
-<<<<<<< HEAD
-from ttlexp.mfg import mfg_bd as mfg_api
-from ttlexp.integration import int_bd as int_api
-from ttlexp.engineering import eng_bd as eng_api
-from tools.utility import utility as utility_api
-from ttlsap.adapter.intrpt import IntRptConnect
-=======
 from ttlexp import create_app
->>>>>>> main
 
 
 app=create_app('flask.cfg')
