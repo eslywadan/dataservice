f55a3389c76faed30350f0869aed8f734112a173
qs.chou@innolux.com <qs.chou@innolux.com>
2022-06-24 08:50:39 +0800

    modified .gitignore

--- api_portal.py
+++ api_portal.py
@@ -2,10 +2,10 @@
 # import werkzeug 
 # import tools.request_handler as req
 from ttlexp import create_app
-
+from flask_cors import CORS
 
 app=create_app('flask.cfg')
-
+CORS(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8060)
